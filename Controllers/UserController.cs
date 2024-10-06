108596a7bfe4894041d45dad8f1178945c902ad0
FernandoZuchi <nandozuchi07@gmail.com>
2024-10-06 01:22:42 -0300

    M1A7 - Projeto estruturado, primeiro endpoint criado

---
+++ Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_C__API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var response = new Response
+        {
+            Name = "Fernando",
+            Age = 0,
+        };
+
+        return Ok(response);
+    }
+}
---
+++ Response.cs
@@ -0,0 +1,7 @@
+﻿namespace dotnet_C__API;
+
+public class Response
+{
+    public String Name { get; set; }
+    public int Age { get; set; }
+}
